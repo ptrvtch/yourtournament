2ec6672ff2386009a4cd275e7fa78472d9ebe336
Oleg Omelchenko <ptrvtch@gmail.com>
2016-07-07 17:19:46 +0300

    add localstorage support and interceptors

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -2,13 +2,22 @@
     'use strict';
 
     angular
-        .module('yt', ['ngCookies', 'ngRoute', 'ui.bootstrap'])
+        .module('yt', ['ngCookies', 'ngRoute', 'ngStorage', 'ui.bootstrap'])
         .config(config)
         .run(run);
 
-    config.$inject = ['$interpolateProvider', '$locationProvider', '$routeProvider'];
+    config.$inject = [
+        '$interpolateProvider',
+        '$locationProvider',
+        '$routeProvider',
+        '$httpProvider'
+    ];
 
-    function config($interpolateProvider, $locationProvider, $routeProvider) {
+    function config($interpolateProvider,
+                    $locationProvider,
+                    $routeProvider,
+                    $httpProvider
+    ) {
         $interpolateProvider.startSymbol('{$');
         $interpolateProvider.endSymbol('$}');
 
@@ -29,6 +38,29 @@
             }
         )
             .otherwise('/');
+
+        $httpProvider.interceptors.push([
+            '$q',
+            '$location',
+            '$localStorage',
+            function($q, $location, $localStorage) {
+                return {
+                    'request': function(config) {
+                        config.headers = config.headers || {};
+                        if ($localStorage.token) {
+                            config.headers.Authorization = 'Bearer ' + $localStorage.token;
+                        }
+                        return config;
+                    },
+                    'responseError': function(response) {
+                        if (response.status === 401 || response.status === 403) {
+                            $location.path('/signin');
+                        }
+                        return $q.reject(response);
+                    }
+                }
+            }
+        ])
     }
     
     run.$inject = ['$http'];
